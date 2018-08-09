33459fff458c2eaae008e5f386da95e2c2abd226
Alex S <alex.sunga@fliptech.co>
2018-08-09 05:08:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const rankLists = document.querySelectorAll('.ranked-list')
+  
+  for (let i= 0, l = rankLists.length; i < l; i++){
+    let children = rankLists[i].children
+  }
+  
+  for (let j = 0, k = children.length; j < k; j++){
+    children[j].innerHTML = parseInt(children[j].innerHTML) + n
+  }
+  
+}
+
+function deepestChild(){
+  let grandNode = document.getElementById('grand-node')
+  let gGrandNode = node.children[0]
+  
+  while (gGrandNode){
+  grandNode = gGrandNode
+  gGrandNode = grandNode.children[0]
+  }
+  return grandNode
+}
